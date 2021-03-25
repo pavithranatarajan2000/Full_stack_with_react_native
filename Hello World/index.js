f09acd4ecbb7f28db7e580894657630201d20950
pavithranatarajan2000 <58357646+pavithranatarajan2000@users.noreply.github.com>
2021-03-24 22:34:18 -0400

    Create index.js

---
+++ Hello World/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+const element= <h1> Hello world</h1>;
+console.log(element);
+ReactDOM.render(element, document.getElementById('root'));
